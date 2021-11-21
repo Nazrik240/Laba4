b5e9a4f0c2b455d6f54b1b6118e8cc84ce0b7bc3
Nazrik240 <90559607+Nazrik240@users.noreply.github.com>
2021-11-21 15:31:30 +0500

    Доработал программу

---
+++ Program123.pas
@@ -0,0 +1,53 @@
+﻿var H1 :array[1..1000] of integer; //Индекс таксиста
+M1 :array[1..1000] of integer; //Индекс сотрудника
+H :array[1..1000] of integer; //Тариф таксиста
+M :array[1..1000] of integer; //Растояния до дома сотрудника
+j,N,i,k:integer;
+begin
+writeln('Введите количество сотрудников');
+
+try
+var A:=Readlninteger(); // Ввод количества сотрудников
+N:=A;
+k:=0;
+begin
+writeln('Введите растояния до дома для каждого сотрудника'); 
+for i:=1 to N do read(M[i]); //Ввод расстояния
+writeln('Введите тарифы каждого таксиста'); 
+for i:=1 to N do read(H[i]); //Ввод тарифа
+for i:=1 to N do
+begin
+if k<N then k:=k+1;
+H1[i]:=k;
+M1[i]:=k;
+end;
+end;
+      begin // Сортировка расстояния
+ for i:=1 to N-1 do 
+        for j:=1 to N-i do
+            if M[j] > M[j+1] then begin
+                Swap(M[j],M[j+1]);
+               Swap(M1[j],M1[j+1])
+                end;      
+       end;
+       begin
+ for i:=1 to N-1 do // Сортировка тарифа
+        for j:=1 to N-i do
+            if H[j] < H[j+1] then begin
+                Swap(H[j],H[j+1]);
+               Swap(H1[j],H1[j+1])
+                end;      
+       end;
+begin 
+  for i:=1 to N do  writeln('Сотрудник № ',M1[i],' Поедет на такси № ',H1[i]);
+    writeln(' ');
+end;
+
+
+
+except
+writeln('Некоректный ввод данных, пожалуйста перезапустите програму и повторите ввод');
+end;
+end.
+
+  
